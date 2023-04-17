03656c9cec4314b4db01e22f7a4ea72bdc7e85ef
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2023-04-17 14:16:14 +0100

    update

--- server.ts
+++ server.ts
@@ -1,6 +1,10 @@
 import { express } from 'express'
 const app = express();
 
+app.get("/", () => {
+  
+})
+
 const listener = app.listen(process.env.PORT, () => {
   let address = listener.address()
   console.log("Your app is listening on port " + address.port);
